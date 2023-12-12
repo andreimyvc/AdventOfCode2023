f97a708044890da0bde0481b5f6e9790c55ebed1
CVilorio <cvilorio@outlook.com>
2023-12-11 23:55:56 -0400

    First commit

---
+++ src/Day_01/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Day_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] data = File.ReadAllLines("day01.1.txt");
+            int sum = Calibrate(data);
+
+            Console.WriteLine($"Calibration : {sum}");
+            Console.ReadKey();
+        }
+
+        private static int Calibrate(string[] data)
+        {
+            return data.Select(p => p.ToArray().Where(c => char.IsDigit(c)))
+                .Sum(p => int.Parse(p.FirstOrDefault() + "" + p.LastOrDefault()));
+        }
+    }
+}
