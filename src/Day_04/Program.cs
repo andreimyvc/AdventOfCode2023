b08c7d3b1e7708b4c4f9288637593c85ad5627d7
CVilorio <cvilorio@outlook.com>
2023-12-29 13:43:15 -0400

    Dia 04 parte 2 completa

--- src/Day_04/Program.cs
+++ src/Day_04/Program.cs
@@ -6,7 +6,7 @@
         {
             string[] data = File.ReadAllLines("input.txt");
             Console.WriteLine($"Part 01:{Part1(data)}");
-            Console.WriteLine($"Part 02:{Part2(data)}");
+            Console.WriteLine($"Part 02:{Part2(data.Skip(0).ToArray())}");
             Console.WriteLine(lista.Count);
             lista.Order().ToList().ForEach(x => Console.WriteLine(x));
         }
@@ -36,7 +36,7 @@
         }
 
         static List<string> lista = new List<string>();
-        static int Part2(string[] data)
+        static int Part2Old(string[] data)
         {
             lista.AddRange(data);
             int sum = 0, temp = 0;
@@ -73,6 +73,54 @@
             }            
 
             return sum;
+        }
+
+        static int Part2(string[] data)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                var line = data[i];
+                var arr = line.Split('|');
+                var winner = arr[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var games = arr[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                int temp = 0;
+
+                foreach (var b in games)
+                {
+                    if (winner.Contains(b))
+                    {
+                        temp++;
+                    }
+                }
+
+                int n = 0;
+                int key = 0;
+                int y = 0;
+
+                int next = 0;
+
+                while (y < temp)
+                {
+                    key = i + y + 1;
+
+                    if (n == 0)
+                    {
+                        result.TryGetValue(key, out n);
+                        n += 1;
+                    }
+
+                    result.TryGetValue(key + 1, out next);
+
+                    result[key + 1] = next + n;
+
+                    y++;
+                }
+            }
+
+            return result.Sum(p => p.Value) + data.Length;
         }
     }
 }
